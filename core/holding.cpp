9023737921e81b3a1eae2f87178ed45a7d95c588
Pi <altmanpp@gmail.com>
2023-12-20 23:03:00 +0000

    feat: added the holding core functions

--- core/holding.cpp
+++ core/holding.cpp
@@ -1,1 +1,10 @@
+#include "header/holding.h"
 
+Holding::Holding(Stock *s) {
+    stock_ptr = s;
+    buy_price = s->cur_price;
+}
+
+void Holding::sell() {
+    sell_price = stock_ptr->cur_price;
+}
