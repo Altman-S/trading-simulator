589e64b0c2c8517a9afb257d7e52a13c6fd0ed73
Pi <altmanpp@gmail.com>
2023-12-25 19:52:44 +0000

    feat: added the knapsack algorithm signature

---
+++ core/header/knapsack_solve.h
@@ -0,0 +1,8 @@
+#ifndef KNAPSACK_H
+#define KNAPSACK_H
+
+#include <vector>
+
+std::vector<int> knapsack_solve(int capacity, int n, std::vector<int> weight, std::vector<float> value);
+
+#endif
---
+++ core/knapsack_solve.cpp
@@ -0,0 +1,5 @@
+#include "header/knapsack_solve.h"
+
+std::vector<int> knapsack_solve(int capacity, int n, std::vector<int> weight, std::vector<float> value) {
+    return {};
+}
